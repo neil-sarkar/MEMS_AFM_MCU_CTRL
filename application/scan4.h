b823a424c62656f3cdf7c8f1edd487ef5fbd46b9
molfat <mahdiolfat@gmail.com>
2014-11-13 18:12:15 -0500

    more clean-up

--- application/scan4.h
+++ application/scan4.h
@@ -13,6 +13,5 @@ void scan4_get_data (void);
 void scan4_start (void);
 void scan4_step (void);
 bool scan4_get_dac_data (void);
-//void scan4_send_dac_data (void);
 void s4_set_sample_cnt (u8 smaple_cnt);
 void s4_set_dwell_t_ms (u8 dwell_ms);
