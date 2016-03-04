0eaaacc9be7469eab8bbd31b9472ec35055e12c5
brohanlo <brohanlo@uwaterloo.ca>
2016-03-04 13:24:27 -0500

    tracking algorithm uses RMS detector for amplitude and PLL front-end for phase

--- application/um.c
+++ application/um.c
@@ -43,7 +43,7 @@ void um_init (void)
 #define DAC_HORZ	DAC_ZOFFSET_COARSE	
 #define DAC_L1		DAC_X1
 #define DAC_L2		DAC_Y1
-#define UM_delay 	1
+#define UM_delay 	100
 #define COM_delay 	100
 
 //pid variables
@@ -82,6 +82,8 @@ static u16 threshold=2000;
 static u16 prevMax=0;
 static s32 DIR=1;
 static u16 vertdata;
+static s32 peak_amp_val=0;
+static s32 peak_phase_val=0;
 	
 void um_track (void)
 {
@@ -98,175 +100,46 @@ void um_track (void)
 		um.horz.iMax = 0;
 		um.horz.iMin = 4095;
 		edgenum=0;
-		/*
+		peak_amp_val=0;
 		
-		for (i = 1000; i < 4095; i += 5)
+		for (i = 0; i < 64; i++)
 		{
-			delay = UM_delay;
-			while (delay--);
-			if (i > 4095) i = 4095;
-			dac_set_val(DAC_HORZ, i);
-			adc_start_conv(ADC_MIRROR);
-			//val = adc_get_avgw_val(8, 100);
-			val = adc_get_val();
-			
-			if (val > um.horz.iMax) 
-			{
-				um.horz.iMax = val;
-			}
-			if (val < um.horz.iMin) 
-			{
-				um.horz.iMin = val;
-			}
-			if (!triggered)
-			{
-				if (val>threshold)
-				{
-					threshold=threshold-hysteresis;
-					triggered = true;
-					trigpos[edgenum]=i;
-					edgenum++;
-				}
-			}
-			if (triggered)
-			{	
-				if (val<threshold)
-				{
-					threshold=threshold+hysteresis;
-					triggered = false;
-				}
- 			}
-		}
-//		um.horz.max_b = 0;
-//		delay = 1000;
-//		while(delay--);
-		for (i = 4095; i > 1000; i -= 5)
-		{
-			delay = UM_delay;
-			while (delay--);
-			if (i < 0) i = 0;
-			dac_set_val(DAC_HORZ, i);
-			adc_start_conv(ADC_MIRROR);
-			//val = adc_get_avgw_val(8, 100);
-			val = adc_get_val();
-			if (val > um.horz.iMax) 
-			{
-				um.horz.iMax = val;
-			}
-			if (val < um.horz.iMin) 
-			{
-				um.horz.iMin = val;
-			}
-			if (!triggered)
-			{
-				if (val>threshold)
-				{
-					threshold=threshold-hysteresis;
-					triggered = true;
-					trigpos[edgenum]=i;
-					edgenum++;
-				}
-			}
-			if (triggered)
-			{	
-				if (val<threshold)
-				{
-					threshold=threshold+hysteresis;
-					triggered = false;
-				}
- 			}
-		}
-
-		um.horz.peak_posn = (trigpos[0] + trigpos[1]) / 2;
-		um.horz.peak_posn = 4095 - um.horz.peak_posn; 
-		
-		//dac_set_val(DAC_HORZ, um.horz.peak_posn);
-		
-		range = um.horz.iMax - um.horz.iMin;
-		threshold = um.horz.iMin + .7*range;
-		hysteresis = range/10;
-		*/
-		if((range<=350)&&(exitFlag==0))		//recovery loop; sparse raster scan
-		{
-		vertpos+=DIR*(scan_numpts/32);
-		if(vertpos>(scan_numpts-1))  
+			vertpos+=DIR*(scan_numpts/64);
+			if(vertpos>(scan_numpts-1))  
 			{	
 				DIR=DIR*-1;
 				vertpos=scan_numpts-1;
 			}
-		if(vertpos<0) 
+			if(vertpos<0) 
 			{	
-			DIR=DIR*-1;
-			vertpos=0;
+				DIR=DIR*-1;
+				vertpos=0;
 			}
-		iTerm=0;
 			
-		if (vertpos > scan_numpts) 	vertpos = scan_numpts;
-		if (vertpos < 0) 						vertpos = 0;
-	
-		dac_set_val(DAC_X1, scan_r_points[vertpos]);
-		dac_set_val(DAC_Y1, scan_l_points[vertpos]);
-		
+			//if (vertpos > scan_numpts) 	vertpos = scan_numpts;
+			//if (vertpos < 0) 						vertpos = 0;
+			dac_set_val(DAC_X1, scan_r_points[vertpos]);
+			dac_set_val(DAC_Y1, scan_l_points[vertpos]);
+			delay = UM_delay;
+			while (delay--);
+			adc_start_conv(ADC_MIRROR);
+			val = adc_get_val();
+			if (val>peak_amp_val)
+				
+			{	peak_amp_val=val;
+				vertdata = vertpos;
+				adc_start_conv(ADC_PHASE);
+				peak_phase_val = adc_get_val();
+			}
 		}
-		else  //vertical tracking
-		{
-			vertshift=(vertpos - prevVertPos);
-				if (vertshift==0)
-				{
-					vertshift=1;
-				}
-			fb = ((float)(range - prevRange)) / (vertshift);
-			error = setpoint - fb;
-			iTerm += (ki * error);
-			if (iTerm >= scan_numpts/2) iTerm=scan_numpts/2;
-		//if (iTerm > scan_numpts-1) iTerm = scan_numpts-1;
-			if (iTerm <= -1*scan_numpts/2) iTerm= -1*scan_numpts/2;
 		
-		pidval = (u16)(kp * error + iTerm);
-		if(pidval==0) pidval=-1*DIR;  //to do: make this direction dependant
-		
-		// Vertical Tracking
-		//should we use range or max value to determine whether to switch directions?
-		if (um.horz.iMax>prevMax)
-		//if (range>prevRange)
-		
-		{
-			if (dir>0) vertpos += pidval;
-			if (dir<0) vertpos -= pidval;					
-		}
-		else
-		{
-			dir=dir*-1;
-			if (dir>0) vertpos += pidval;
-			if (dir<0) vertpos -= pidval; //when the pistons change directions, reset the iTerm?
-			//iTerm=0;
-			//if (dir>0) vertpos += 1;
-			//if (dir<0) vertpos -= 1;
-			
-		}				  
-
-		if (vertpos > scan_numpts) 	vertpos = scan_numpts;
-		if (vertpos < 0) 						vertpos = 0;	
-	
-		dac_set_val(DAC_X1, scan_r_points[vertpos]);
-		dac_set_val(DAC_Y1, scan_l_points[vertpos]);
-		}
-		// delay
-		delay = UM_delay;
-		while (delay--);
-
-		// set voltage to maximum
-		// dac_set_val(DAC_VERT, dac_avg);
-
+		um.horz.peak_posn=peak_phase_val;
+		//vertdata=peak_amp_val;
 		uart_set_char (um.horz.peak_posn);
 		uart_set_char (um.horz.peak_posn >> 8);
-		vertdata = (scan_numpts-vertpos)*4095/scan_numpts; // scale from 0 to 3.3
+		vertdata = (scan_numpts-vertdata)*4095/scan_numpts; // scale from 0 to 3.3
 		uart_set_char (vertdata);
-		uart_set_char (vertdata >> 8);	
-		
-		prevMax			= um.horz.iMax;
-		prevRange 	= range;
-		prevVertPos = vertpos;
+		uart_set_char (vertdata >> 8);
 		
 	}
 	exitFlag = 0;
